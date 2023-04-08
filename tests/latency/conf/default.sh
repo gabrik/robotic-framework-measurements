4d4230f03b65fcf47cafa85d2166daf87f3e5196
gabrik <gabrik@gabrielebaldoni.com>
2023-04-08 11:43:03 +0000

    Added latency test for single rate
    Signed-off-by: gabrik <gabrik@gabrielebaldoni.com>

--- tests/latency/conf/default.sh
+++ tests/latency/conf/default.sh
@@ -4,6 +4,8 @@ TEST_LOGS="${TEST_LOGS:-log}"
 INITIAL_MSGS=1
 FINAL_MSGS=${FINAL_MSGS:-1000000}
 
+SEND_MSGS=${SEND_MSGS:-1}
+
 #WORKING_DIR="test-ws"
 WORKING_DIR="Workspace"
 PERF_GIT="https://github.com/gabrik/robotic-framework-measurements"
